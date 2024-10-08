7d258742e12f3ab1d3a7f7dd2f1cfaf6aa971bb1
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-10-09 00:47:25 +0700

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Ricky Primayuda Putra on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
